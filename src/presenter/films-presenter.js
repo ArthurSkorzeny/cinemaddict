53ceead87df05fc40e62d71a1fe7d40821826a86
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-08-12 19:12:33 +0300

    Add files via upload

---
+++ src/presenter/films-presenter.js
@@ -0,0 +1,34 @@
+import SortButtonsView from '../view/sort-view.js';
+import NavigationButtonsView from '../view/films-navigation-view.js';
+import FilmsView from '../view/films-view.js';
+import ShowMoreButtonView from '../view/show-more-view.js';
+import FilmsListView from '../view/film-list-view.js';
+import FilmListContainerView from '../view/film-list-container-view.js';
+import FilmCardView from '../view/film-card-view.js';
+import {render} from '../render.js';
+
+
+export default class PagePresenter {
+  filmsComponent = new FilmsView();
+  filmList = new FilmsListView();
+  filmListContainer = new FilmListContainerView();
+
+
+  init = (pageContainer) => {
+    this.pageContainer = pageContainer;
+
+    render(new NavigationButtonsView(), pageContainer);
+    render(new SortButtonsView(), pageContainer);
+
+    render(this.filmsComponent, pageContainer);
+    render(this.filmList, this.filmsComponent.getElement());
+    render(this.filmListContainer, this.filmList.getElement());
+
+    for (let i = 0; i < 5; i++) {
+      render(new FilmCardView(), this.filmListContainer.getElement());
+    }
+
+
+    render(new ShowMoreButtonView(), this.filmList.getElement());
+  };
+}
