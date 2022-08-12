de250b9a286e3d25e7c910017f21f5ac33bf97bc
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-08-12 19:12:13 +0300

    Update main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import UserProfileView from './view/header-profile-view.js';
+import FooterStatisticView from './view/footer-statistics-view.js';
+import PagePresenter from './presenter/films-presenter.js';
+import {render} from './render.js';
+
+const siteHeaderElement = document.querySelector('.header');
+const siteMainElement = document.querySelector('.main');
+const siteFooterElement = document.querySelector('.footer');
+const siteFooterStatistic = siteFooterElement.querySelector('.footer__statistics');
+const pagePresenter = new PagePresenter();
+
+render( new UserProfileView(), siteHeaderElement);
+render( new FooterStatisticView(), siteFooterStatistic);
+
+pagePresenter.init(siteMainElement);
