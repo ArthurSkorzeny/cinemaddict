af2b1d46dbf8f37bf9989b810f0e562b9dd9dca0
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-08-18 17:34:17 +0300

    Update card.js

--- src/mock/card.js
+++ src/mock/card.js
@@ -1,4 +1,4 @@
-import {generateRandomPart} from '../utils.js';
+import {generateRandomPart, getRandomInteger} from '../utils.js';
 
 const posters = [
   'made-for-each-other.png',
@@ -10,11 +10,42 @@ const posters = [
   'the-man-with-the-golden-arm.jpg',
 ];
 
+const titles = [
+  'A Little Pony Without The Carpet',
+  'Made for Each Other',
+  'Sagebrush Trail',
+  'The Great Flamarion',
+  'Santa Claus Conquers the Martians',
+  'The Dance of Life',
+  'The Man with the Golden Arm',
+];
+
+const genres = [
+  'Action',
+  'Comedy',
+  'Adventure',
+  'Drama',
+  'Animated',
+  'Horror',
+  'Fantasy',
+];
+
+const descriptions = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis.',
+];
+
+
 export const generateFilmCard = () => ({
-  'id': '1',
-  'comments': [1, 2, 3],
+  'id': getRandomInteger(1, 100),
+  'comments': Array.from({length: getRandomInteger(2, 45)}, () => Math.floor(Math.random() * getRandomInteger(2, 45))),
   'filmInfo': {
-    'title': 'A Little Pony Without The Carpet',
+    'title': generateRandomPart(titles),
     'alternativeTitle': 'Laziness Who Sold Themselves',
     'totalRating': 5.3,
     'poster': `./images/posters/${generateRandomPart(posters)}`,
@@ -27,19 +58,19 @@ export const generateFilmCard = () => ({
       'Morgan Freeman'
     ],
     'release': {
-      'date': '2019-05-11T00:00:00.000Z',
+      'date': `${getRandomInteger(1954, 2022)}-05-11T00:00:00.000Z`,
       'releaseCountry': 'Finland'
     },
-    'runtime': 77,
+    'runtime': getRandomInteger(60,134),
     'genre': [
-      'Comedy'
+      generateRandomPart(genres)
     ],
-    'description': 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland", with the best fight scenes since Bruce Lee.'
+    'description': generateRandomPart(descriptions)
   },
   'userDetails': {
-    'watchlist': false,
-    'alreadyWatched': true,
+    'watchlist': Boolean(getRandomInteger(0, 1)),
+    'alreadyWatched': Boolean(getRandomInteger(0, 1)),
     'watchingDate': '2019-04-12T16:12:32.554Z',
-    'favorite': false
+    'favorite': Boolean(getRandomInteger(0, 1))
   }
 });
